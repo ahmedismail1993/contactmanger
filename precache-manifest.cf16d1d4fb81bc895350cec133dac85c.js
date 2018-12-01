ecd62b5447ee0f00c42a860ab555cb11213695ff
unknown <myemailid@domain.com>
2018-12-02 00:44:45 +0200

    Updates

---
+++ precache-manifest.cf16d1d4fb81bc895350cec133dac85c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f92335250aa881191a97",
+    "url": "/contactmanger/static/js/runtime~main.f9233525.js"
+  },
+  {
+    "revision": "ca940da95d97cafd8127",
+    "url": "/contactmanger/static/js/main.ca940da9.chunk.js"
+  },
+  {
+    "revision": "aa62b19a731925790440",
+    "url": "/contactmanger/static/js/1.aa62b19a.chunk.js"
+  },
+  {
+    "revision": "ca940da95d97cafd8127",
+    "url": "/contactmanger/static/css/main.e8ae1d21.chunk.css"
+  },
+  {
+    "revision": "aa62b19a731925790440",
+    "url": "/contactmanger/static/css/1.828f1b60.chunk.css"
+  },
+  {
+    "revision": "213bc4a8b98a51a6337b46ce5492d36c",
+    "url": "/contactmanger/index.html"
+  }
+];
